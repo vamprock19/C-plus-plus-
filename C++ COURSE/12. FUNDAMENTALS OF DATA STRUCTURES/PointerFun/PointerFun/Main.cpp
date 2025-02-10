d35f6a8f7a67d3556707061aae75614873d06f69
vamprock19 <vamprock19@gmail.com>
2025-02-10 16:48:17 -0500

    introduction to pointers update

--- C++ COURSE/12. FUNDAMENTALS OF DATA STRUCTURES/PointerFun/PointerFun/Main.cpp
+++ C++ COURSE/12. FUNDAMENTALS OF DATA STRUCTURES/PointerFun/PointerFun/Main.cpp
@@ -4,12 +4,20 @@ using namespace std;
 int main()
 {
 	int myLovelyInt = 150;
-	int* somePtr = &myLovelyInt; // & used to get the address
+	double myDouble = 3.14;
+
+	int* somePtr = &myLovelyInt; // & used to get the address. 
+	double* doublePtr = &myDouble;
+
+
 	cout << "mylovelyInt is originally: " << myLovelyInt << endl;
 	cout << "pointer holds value: " << somePtr << endl; 
-	cout << "pointer dereferenced: " << *somePtr << endl;
+	cout << "pointer dereferenced: " << *somePtr << endl; // dereferenced
 	*somePtr = 200;
 
 	cout << "myLovelyInt is now: " << myLovelyInt << endl;
+	
+	cout << doublePtr << endl;
+	cout << *doublePtr << endl;
 	return 0;
 }

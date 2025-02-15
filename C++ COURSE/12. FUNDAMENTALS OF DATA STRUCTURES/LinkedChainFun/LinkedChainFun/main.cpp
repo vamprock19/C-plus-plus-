65759b1d9cedbffeac2a41292abda3072169554a
vamprock19 <vamprock19@gmail.com>
2025-02-15 11:43:20 -0500

    LinkedChainFun

---
+++ C++ COURSE/12. FUNDAMENTALS OF DATA STRUCTURES/LinkedChainFun/LinkedChainFun/main.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include"Node.h"
+
+using namespace std;
+
+Node* createChain();
+void deleteChain(Node*& head);
+void printChain(Node* head);
+
+int main()
+{
+	Node* theHead;
+	theHead = createChain();
+	printChain(theHead);
+	deleteChain(theHead);
+
+	return 0;
+}
+
+Node* createChain()
+{
+	Node* head = nullptr;
+
+	for (int i = 0; i < 25; i++)
+	{
+		head = new Node(i, head);
+	}
+	return head;
+}
+void deleteChain(Node*& head)
+{
+	Node* nodeToDelete;
+	while (head != nullptr)
+	{
+		nodeToDelete = head;
+		head = head->getNext();
+		delete nodeToDelete;
+	}
+}
+void printChain(Node* head)
+{
+	Node* walker = head;
+	while (walker != nullptr)
+	{
+		cout << walker->getData() << endl;
+		walker = walker->getNext();
+	}
+}
